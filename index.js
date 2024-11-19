fa33ea6c8817c87ffa9d849e3710276065755ce9
hash-ir <hashirahmad@zhcet.ac.in>
2024-11-19 14:50:51 +0100

    ex3.7-3.8: add partial solutiion of part3

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', function (req) {
+    if (req.method === 'POST') {
+        return JSON.stringify(req.body)
+    } else {
+        return null
+    }
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
